463496abfd02bde11a456d124d9f451e65f63e17
leon <leonbarroso@gmail.com>
2016-01-06 15:47:17 -0400

    make it better

--- web/index.php
+++ web/index.php
@@ -7,86 +7,86 @@ $app['debug'] = true;
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
+    'monolog.logfile' => 'php://stderr',
 ));
 
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+    'twig.path' => __DIR__ . '/views',
 ));
 
 // inicial
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+$app->get('/', function () use ($app) {
+    $app['monolog']->addDebug('logging output.');
+    return $app['twig']->render('index.twig');
 });
 
 
 //conexão com banco de dados
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $app->register(new Herrera\Pdo\PdoServiceProvider(),
-  array(
-    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
-    'pdo.port' => $dbopts["port"],
-    'pdo.username' => $dbopts["user"],
-    'pdo.password' => $dbopts["pass"]
-  )
+    array(
+        'pdo.dsn' => 'pgsql:dbname=' . ltrim($dbopts["path"], '/') . ';host=' . $dbopts["host"],
+        'pdo.port' => $dbopts["port"],
+        'pdo.username' => $dbopts["user"],
+        'pdo.password' => $dbopts["pass"]
+    )
 );
 
 // teste de bd
-$app->get('/db/', function() use($app) {
-  $st = $app['pdo']->prepare('SELECT * FROM test_table');
-  $st->execute();
+$app->get('/db/', function () use ($app) {
+    $st = $app['pdo']->prepare('SELECT * FROM test_table');
+    $st->execute();
 
-  $names = array();
-  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-    $app['monolog']->addDebug('Row ' . $row['name']);
-    $names[] = $row;
-  }
+    $names = array();
+    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+        $app['monolog']->addDebug('Row ' . $row['name']);
+        $names[] = $row;
+    }
 
-  return $app['twig']->render('database.twig', array(
-    'names' => $names
-  ));
+    return $app['twig']->render('database.twig', array(
+        'names' => $names
+    ));
 });
 
 //  ----------------- app propriamente dito ----------------------
 
 // inicial pesquisa
-$app->get('/itriad/', function() use($app) {
-  $st = $app['pdo']->prepare('SELECT * FROM funcionario');
-  $st->execute();
+$app->get('/itriad/', function () use ($app) {
+    $st = $app['pdo']->prepare('SELECT * FROM funcionario');
+    $st->execute();
 
-  $names = array();
-  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-    $app['monolog']->addDebug('Row ' . $row['name']);
-    $names[] = $row;
-  }
+    $names = array();
+    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+        $app['monolog']->addDebug('Row ' . $row['name']);
+        $names[] = $row;
+    }
 
-  return $app['twig']->render('pesquisa.twig', array(
-    'names' => $names
-  ));
+    return $app['twig']->render('pesquisa.twig', array(
+        'names' => $names
+    ));
 });
 
 // editar registro
-$app->get('/editar/{id}', function($id) use($app) {
-  $st = $app['pdo']->prepare('SELECT * FROM funcionario where id='.$id);
-  $st->execute();
+$app->get('/editar/{id}', function ($id) use ($app) {
+    $st = $app['pdo']->prepare('SELECT * FROM funcionario where id=' . $id);
+    $st->execute();
 
-  $names = array();
-  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-    $app['monolog']->addDebug('Row ' . $row['name']);
-    $names[] = $row;
-  }
+    $names = array();
+    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+        $app['monolog']->addDebug('Row ' . $row['name']);
+        $names[] = $row;
+    }
 
-  return $app['twig']->render('editar.twig', array(
-      'names' => $names
-  ));
+    return $app['twig']->render('editar.twig', array(
+        'names' => $names
+    ));
 });
 
 //adicionar novo
-$app->get('/editar/', function() use($app) {
-  return $app['twig']->render('criar.twig');
+$app->get('/editar/', function () use ($app) {
+    return $app['twig']->render('criar.twig');
 });
 
 
@@ -94,22 +94,37 @@ $app->get('/editar/', function() use($app) {
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-$app->post('/salvar/{id}', function(Request $request, $id) use($app) {
-  $nome = $request->get('nome');
-  $cpf = $request->get('cpf');
-  $endereco = $request->get('endereco');
-  $data_nascimento = $request->get('data_nascimento');
-  $sql = 'UPDATE funcionario
-       SET  cpf = '.$cpf.',
-            nome = \''.$nome.'\',
-            endereco = \''.$endereco.'\',
-            data_nascimento = \''.$data_nascimento.'\'
-      where id='.$id;
-  $st = $app['pdo']->prepare($sql);
-  $st->execute();
+$app->post('/salvar/{id}', function (Request $request, $id) use ($app) {
+    $nome = $request->get('nome');
+    $cpf = $request->get('cpf');
+    $endereco = $request->get('endereco');
+    $data_nascimento = $request->get('data_nascimento');
+    $sql = 'UPDATE funcionario
+       SET  cpf = ' . $cpf . ',
+            nome = \'' . $nome . '\',
+            endereco = \'' . $endereco . '\',
+            data_nascimento = \'' . $data_nascimento . '\'
+      where id=' . $id;
+    $st = $app['pdo']->prepare($sql);
+    $st->execute();
 
-  //return new Response($sql, 201);
-  return $app->redirect("/editar/$id");
+    //return new Response($sql, 201);
+    return $app->redirect("/editar/$id");
+});
+
+//criat novo registro
+$app->post('/salvar/', function (Request $request) use ($app) {
+    $nome = $request->get('nome');
+    $cpf = $request->get('cpf');
+    $endereco = $request->get('endereco');
+    $data_nascimento = $request->get('data_nascimento');
+    $sql = 'INSERT into funcionario (cpf,nome,endereco,data_nascimento) VALUES (' . $cpf . ',\'' . $nome . '\',\'' . $endereco . '\',\'' . $data_nascimento . '\'';
+    $st = $app['pdo']->prepare($sql);
+    $st->execute();
+    $id = $app['pdo']->lastInsertId();
+
+    //return new Response($sql, 201);
+    return $app->redirect("/editar/$id");
 });
 
 
