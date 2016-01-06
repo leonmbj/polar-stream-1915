027eeacc805a31bab1ac5e8bcc049d6b746ba092
leon <leonbarroso@gmail.com>
2016-01-06 15:11:47 -0400

    make it better

--- web/index.php
+++ web/index.php
@@ -91,11 +91,11 @@ $app->get('/editar/', function() use($app) {
 
 
 // salvar registro editado
-$app->match('/salvar/{id}', function(Request $request, $id) use($app) {
-  $nome = $request->get('nome');
-  $cpf = $request->get('cpf');
-  $endereco = $request->get('endereco');
-  $data_nascimento = $request->get('data_nascimento');
+$app->post('/salvar/{id}', function($_POST, $id) use($app) {
+  $nome = $_POST['nome'];
+  $cpf = $_POST['cpf'];
+  $endereco = $_POST['endereco'];
+  $data_nascimento = $_POST['data_nascimento'];
   $st = $app['pdo']->prepare(
       'UPDATE funcionario
        SET  cpf = '.$cpf.',
